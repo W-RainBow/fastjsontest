9484c21cbc14201153d897ac4b72f32706f9f287
W-RainBow <40012144+W-RainBow@users.noreply.github.com>
2019-10-25 14:33:59 +0800

    Initial commit

---
+++ src/main/java/com/example/fastjsontest/FastjsontestApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastjsontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastjsontestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastjsontestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fastjsontest/FastjsontestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastjsontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastjsontestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
